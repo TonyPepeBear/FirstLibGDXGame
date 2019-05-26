8c6c23de26d92e29be5e09a15feaee609e700ddf
TonyPepe <tonybear55665566@gmail.com>
2019-05-26 16:08:50 +0800

    Raindrop

--- core/src/com/tonypepe/game/Drop.kt
+++ core/src/com/tonypepe/game/Drop.kt
@@ -9,8 +9,10 @@ import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.Rectangle
 import com.badlogic.gdx.math.Vector3
-import java.awt.Rectangle
+import com.badlogic.gdx.utils.TimeUtils
 
 class Drop : ApplicationAdapter() {
     private lateinit var batch: SpriteBatch
@@ -21,6 +23,8 @@ class Drop : ApplicationAdapter() {
     private lateinit var rainMusic: Music
     private lateinit var camera: OrthographicCamera
     private lateinit var bucket: Rectangle
+    private val raindrops = arrayListOf<Rectangle>()
+    private var lastDropTime = 0L
 
     override fun create() {
         batch = SpriteBatch()
@@ -33,10 +37,10 @@ class Drop : ApplicationAdapter() {
         rainMusic.play()
         camera = OrthographicCamera().apply { setToOrtho(false, 800f, 480f) }
         bucket = Rectangle().apply {
-            x = 800 / 2 - 64 / 2
-            y = 20
-            width = 64
-            height = 64
+            x = (800 / 2 - 64 / 2).toFloat()
+            y = 20F
+            width = 64F
+            height = 64F
         }
     }
 
@@ -48,13 +52,31 @@ class Drop : ApplicationAdapter() {
             projectionMatrix = camera.combined
             begin()
             draw(bucketImage, bucket.x.toFloat(), bucket.y.toFloat())
+            raindrops.forEach {
+                draw(dropImege, it.x.toFloat(), it.y.toFloat())
+            }
             end()
         }
+        moveBucket()
+        if (TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop()
+        val removeList = mutableListOf<Rectangle>()
+        raindrops.forEach {
+            it.y -= (200 * Gdx.graphics.deltaTime).toInt()
+            if (it.y + 64 < 0) removeList.add(it)
+            else if (it.overlaps(bucket)) {
+                dropSound.play()
+                removeList.add(it)
+            }
+        }
+        raindrops.removeAll(removeList)
+    }
+
+    private fun moveBucket() {
         if (Gdx.input.isTouched) {
             val touchPos = Vector3()
             touchPos.set(Gdx.input.x.toFloat(), Gdx.input.y.toFloat(), 0f)
             camera.unproject(touchPos)
-            bucket.x = (touchPos.x - 64 / 2).toInt()
+            bucket.x = touchPos.x - 64 / 2
         }
         when {
             Gdx.input.isKeyPressed(Input.Keys.LEFT) -> {
@@ -64,11 +86,22 @@ class Drop : ApplicationAdapter() {
                 bucket.x += (200 * Gdx.graphics.deltaTime).toInt()
             }
         }
-        if (bucket.x < 0) bucket.x = 0 else if (bucket.x > 800 - 64) bucket.x = 800 - 64
+        if (bucket.x < 0) bucket.x = 0F else if (bucket.x > 800 - 64) bucket.x = (800 - 64).toFloat()
     }
 
     override fun dispose() {
         batch.dispose()
         img.dispose()
+    }
+
+    private fun spawnRaindrop() {
+        val raindrop = Rectangle().apply {
+            x = MathUtils.random(0, 800 - 64).toFloat()
+            y = 480F
+            width = 64F
+            height = 64F
+        }
+        raindrops.add(raindrop)
+        lastDropTime = TimeUtils.nanoTime()
     }
 }
